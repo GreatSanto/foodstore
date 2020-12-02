61c920566ff4267c91e875bf37b25853133e247d
GreatSanto <834046+GreatSanto@users.noreply.github.com>
2020-12-02 09:32:23 +0530

    searchbardone

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ function myFunction() {
     }
   }
 }
+function myFunction2() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch2");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu2");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
